96dcdf6cb24e8f57f3e7f596ccb7d436088765b1
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2024-07-29 12:41:11 -0700

    first project

---
+++ Clone/src/App.jsx
@@ -0,0 +1,19 @@
+import Body from "./Body"
+import { Toaster } from 'react-hot-toast'; 
+import MovieDialog from "./MovieDialog";
+ 
+function App() {
+
+ 
+  return (
+    <>
+
+    <Body/>
+       <Toaster/>  
+       <MovieDialog/>
+  
+    </>
+  )
+}
+
+export default App
---
+++ Clone/src/utils/Constant.jsx
@@ -0,0 +1,21 @@
+export const API_END_POINT = "http://localhost:8080/api/v1/user";
+
+const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+ export const options = {method: 'GET', headers: {accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZGJlZjViMDUzNWE3OGYwMjllMTQ0NDE5NTQ4MjM4MCIsInN1YiI6IjY1MDRhMjNkNTllOGE5MDExZWNhYTVjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LTBaAb_2NPRGPr2HeGszyFDP-onLh-fiL7fzmnOFZUg'
+
+ }};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error('error:' + err));
+  
+export const Now_Playing_Movie = "https://api.themoviedb.org/3/movie/now_playing";
+export const Popular_Movie = "https://api.themoviedb.org/3/movie/popular";
+export const Top_Rated_Movie = "https://api.themoviedb.org/3/movie/top_rated";
+export const Upcoming_Movie = "https://api.themoviedb.org/3/movie/upcoming";
+
+export const  SEARCH_MOVIE_URL="https://api.themoviedb.org/3/search/movie?query=";
+
+export const TMDB_IMG_URL = "https://image.tmdb.org/t/p/w500";
